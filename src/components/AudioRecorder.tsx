4bb9836ac1c5725127c1c844afe19e870365c340
zain-23 <zainshahgit@gmail.com>
2024-12-11 19:59:57 +0500

    play and pause the recorded audios

--- src/components/AudioRecorder.tsx
+++ src/components/AudioRecorder.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { Button } from '@/components/ui';
-import { Fragment, useState } from 'react';
+import { Pause, Play } from 'lucide-react';
+import { useRef, useState } from 'react';
 
 const AudioRecorder = () => {
   const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
   const [audioUrl, setAudioUrl] = useState<string[]>([]);
   const [isRecording, setIsRecording] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement[]>([]);
+  const [playingStates, setPlayingStates] = useState<boolean[]>([]);
 
   const startRecording = async () => {
     try {
@@ -21,9 +24,6 @@ const AudioRecorder = () => {
         const audioBlob = new Blob(aChunks, { type: 'audio/wav' });
         const audioUrl = URL.createObjectURL(audioBlob);
         setAudioUrl((prev) => [...prev, audioUrl]);
-        // const audio = new Audio(audioUrl);
-        // setAudioBlob(audioBlob);
-        // audio.play(); // Play audio
       };
 
       setMediaRecorder(recorder);
@@ -40,21 +40,79 @@ const AudioRecorder = () => {
     }
   };
 
+  const handleAudioPlayPause = (index: number) => {
+    // pick current audio
+    // if audio not fount return
+    // toggle the currentAudio state
+    // pause all audio
+    // play current audio
+    const currentAudio = audioRef.current[index];
+    if (!currentAudio) return;
+
+    setPlayingStates((prev) => {
+      const updatedStates = [...prev];
+      updatedStates[index] = !updatedStates[index];
+
+      // pause all audios
+      updatedStates.forEach((_, i) => {
+        if (i !== index && audioRef.current[i]) {
+          audioRef.current[i].pause();
+          updatedStates[i] = false;
+        }
+      });
+
+      if (updatedStates[index]) {
+        currentAudio.play();
+      } else {
+        currentAudio.pause();
+      }
+
+      return updatedStates;
+    });
+  };
   return (
-    <Fragment>
-      <Button
-        onClick={startRecording}
-        disabled={isRecording}
-      >
-        Start Recording
-      </Button>
-      <Button
-        onClick={stopRecording}
-        variant={'destructive'}
-      >
-        Stop Recording
-      </Button>
-    </Fragment>
+    <div className='h-screen flex flex-col gap-y-3 justify-center items-center'>
+      <div className='space-x-2'>
+        <Button
+          onClick={startRecording}
+          disabled={isRecording}
+        >
+          Start Recording
+        </Button>
+        <Button
+          onClick={stopRecording}
+          variant={'destructive'}
+        >
+          Stop Recording
+        </Button>
+      </div>
+      <div className='w-96 space-y-2'>
+        {audioUrl.length > 0 &&
+          audioUrl.map((audio, index) => (
+            <div
+              key={audio}
+              className='w-full h-11 bg-gray-700 rounded-full flex justify-center items-center'
+            >
+              <button
+                onClick={() => handleAudioPlayPause(index)}
+                className='p-1.5 bg-white/20 rounded-full'
+              >
+                {playingStates[index] ? <Pause size={18} /> : <Play size={18} />}
+              </button>
+              <audio
+                src={audio}
+                ref={(el) => {
+                  if (el) {
+                    audioRef.current[index] = el;
+                  }
+                }}
+              >
+                Hello
+              </audio>
+            </div>
+          ))}
+      </div>
+    </div>
   );
 };
 
